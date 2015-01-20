f8b95023c80957f76d547e71f0c204cacb04d247
Raahil Shah <rds46@cam.ac.uk>
2015-01-20 16:10:15 +0000

    Added an Amazon ItemLookup API tester.

---
+++ AmazonAPITest/ItemLookupTest.java
@@ -0,0 +1,75 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+public class ItemLookupTest {
+
+    private static final String AWS_ACCESS_KEY_ID = "AKIAI4LLUAWZMGNUW5NA";
+    private static final String AWS_SECRET_KEY = "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";
+    private static final String ENDPOINT = "webservices.amazon.com";
+
+    private static String ITEM_ID = "";
+    private static String ID_TYPE = "";
+
+    public static void main(String[] args) {
+
+        if (args.length < 2) {
+            System.err.println("Required arguments: IdType ItemId");
+            return;
+        }
+
+        ID_TYPE = args[0];
+        ITEM_ID = args[1];
+
+        SignedRequestsHelper helper;
+        try {
+            helper = SignedRequestsHelper.getInstance(ENDPOINT, AWS_ACCESS_KEY_ID, AWS_SECRET_KEY);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        
+        String requestUrl = null;
+        String content = null;
+
+        System.out.println("Map form example:");
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("Service", "AWSECommerceService");
+        params.put("Version", "2013-08-01");
+        params.put("Operation", "ItemLookup");
+        params.put("IdType", ID_TYPE);
+        params.put("ItemId", ITEM_ID);
+        params.put("ResponseGroup", "EditorialReview");
+        if (!ID_TYPE.equals("ASIN"))
+            params.put("SearchIndex", "All");
+        params.put("AssociateTag", "drupal0a-20");
+
+        requestUrl = helper.sign(params);
+        System.out.println("Signed Request is \"" + requestUrl + "\"");
+        System.out.println();
+
+        content = fetchContent(requestUrl);
+        System.out.println("Product Description: \n" + content);
+        System.out.println();
+    }
+
+    private static String fetchContent(String requestUrl) {
+        String title = null;
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(requestUrl);
+            Node titleNode = doc.getElementsByTagName("Content").item(0);
+            title = titleNode.getTextContent();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return title;
+    }
+
+}
