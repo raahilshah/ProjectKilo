92eb78d9563818a24ad6aa3a15f8755958e48769
Raahil Shah <rds46@cam.ac.uk>
2015-01-27 10:48:11 +0000

    Revert "Create Eclipse project for backend."
    This reverts commit 9443ba94eb8bace31975dbda09bd76e8e50e3380.

--- backend/src/uk/ac/cam/kilo/backend/GlassFormatter.java
+++
@@ -1,6 +0,0 @@
-package uk.ac.cam.kilo.backend;
-
-public abstract class GlassFormatter {
-	//Format the data returned by the NLP module for display on the Glass
-	public abstract String format(String data);
-}
