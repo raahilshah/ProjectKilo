8ce2010529310fcf7e00a95bfdb03538946a27a1
ruhatch <ruhatch@me.com>
2015-01-30 16:10:17 +0000

    Added GET parameter to RESTful service

--- ProjectKilo/src/main/java/uk/ac/cam/cl/kilo/backend/RESTEasyTest.java
+++ ProjectKilo/src/main/java/uk/ac/cam/cl/kilo/backend/RESTEasyTest.java
@@ -2,16 +2,27 @@ package uk.ac.cam.cl.kilo.backend;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 @Path("/test")
 public class RESTEasyTest {
 	
 	@GET
-	public Response simpleResponse() {
+	public Response simpleResponse(@QueryParam("barcodeNo") String barcodeNo) {
 		
-		String responseString = "Welcome to the Project Kilo WebApp";
+		String responseString;
 		
+		if (barcodeNo != null) {
+			
+			responseString = "Your barcode was: " + barcodeNo;
+			
+		} else {
+		
+			responseString = "Please supply a barcode number";
+		
+		}
+			
 		return Response.status(200).entity(responseString).build();
 	}
 }
