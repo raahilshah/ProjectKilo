4c5427676cc4c2539b02421b5ce598cd97510d58
Davide Cultrera <davidec93@gmail.com>
2015-01-31 16:10:18 +0000

    Write Summariser stub; test server

--- StubClasses/LookupHandler.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.cam.kilo.backend;
-
-public abstract class LookupHandler {
-	//Chooses which API to use based on barcodeType and runs the relevant APIClient
-	public abstract ItemInfo chooseAPI(String barcode, String barcodeType);
-
-}
--- stage.sh
+++ stage.sh
@@ -1,7 +1,13 @@
+# Install project and generate .war
 cd ProjectKilo/
 mvn clean install
+
+# Move .war to the server project
 cp target/ProjectKiloWebApp.war ../cultureglasses/files/default/ProjectKiloWebApp.war
+
+# Cleanup and run Vagrant
 cd ../cultureglasses/
 rm .vagrant/machines/default/virtualbox/synced_folders
-vagrant reload --provision
+sudo vagrant reload --provision
+
 cd ..
