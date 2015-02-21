51ecfd9ddbfc525932f519a43462b44da333c64d
Davide Cultrera <davidec93@gmail.com>
2015-02-21 14:18:32 +0000

    Add more license headings in Glass app; refactored BarcodeEye as CultureGlass

--- CultureGlass/app/src/main/java/com/github/barcodeeye/scan/ui/EmbeddedCardLayoutActivity.java
+++ CultureGlass/app/src/main/java/com/github/barcodeeye/scan/ui/EmbeddedCardLayoutActivity.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2015 Group Kilo (Cambridge Computer Lab)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 
 package com.github.barcodeeye.scan.ui;
 
