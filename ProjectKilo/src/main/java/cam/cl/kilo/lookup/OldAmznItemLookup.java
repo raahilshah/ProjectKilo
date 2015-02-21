5282c4119ae5e5d479f3b334d17531b97fd5ef13
Davide Cultrera <davidec93@gmail.com>
2015-02-21 13:14:14 +0000

    Add Apache 2 license headings to our own files

--- ProjectKilo/src/main/java/cam/cl/kilo/lookup/OldAmznItemLookup.java
+++ ProjectKilo/src/main/java/cam/cl/kilo/lookup/OldAmznItemLookup.java
@@ -1,16 +1,31 @@
+/*
+ * Copyright (C) 2015 Group Kilo (Cambridge Computer Lab)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cam.cl.kilo.lookup;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-import java.util.LinkedList;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class OldAmznItemLookup {
 
