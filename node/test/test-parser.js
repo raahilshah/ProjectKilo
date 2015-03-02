066ab67200b0f9a0ecafbb70b66138b6ec4972ec
Joshua Bambrick <jpbambrick@gmail.com>
2015-03-02 03:52:40 +0000

    improve node docs

--- node/test/test-parser.js
+++ node/test/test-parser.js
@@ -1,11 +1,31 @@
+/*
+ * Copyright (C) 2015 Group Kilo (Cambridge Computer Lab)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 // support requirejs
 require("../lib/requirejs/config.js");
 
 var _ = require("underscore"),
+    // ensure mixins are added to underscore
     mixins = require("../lib/underscore/underscore-mixins.js"),
+    // whether or not to show a full error message
     fullErrorOutput = !!process.argv[2],
+    // the string length of a clipped error message
     clippedErrorOutputLength = 100,
+    // counters for the tests which have been run
     testsRan = 0, testsPassed = 0, testsFailed = 0,
+    // run whenever a test has been completed
     testComplete = function () {
         testsRan += 1;
 
@@ -20,16 +40,19 @@ var _ = require("underscore"),
             }
         }
     },
+    // run whenever a test has passed
     testPassed = function (index, moduleName, label) {
         testsPassed += 1;
         outputTestPass(index, moduleName, label)
         testComplete();
     },
+    // run whenever a test has failed
     testFailed = function (index, moduleName, label, res) {
         testsFailed += 1;
         outputTestFail(index, moduleName, label, res)
         testComplete();
     },
+    // outputs information relating to the failure of a test
     outputTestFail = function (index, moduleName, label, expected, actual) {
         console.log("FAILED test " + index + ": " + moduleName + "->" + label);
         console.log("   expected:");
@@ -37,6 +60,7 @@ var _ = require("underscore"),
         console.log("   got:");
         console.log("   " + (fullErrorOutput ? actual : _.stringClip(JSON.stringify(actual), clippedErrorOutputLength, true)));
     },
+    // outputs information relating to the success of a test
     outputTestPass = function (index, moduleName, label) {
         console.log("PASSED test " + index + ": " + moduleName + "->" + label);
     },
@@ -50,6 +74,7 @@ var _ = require("underscore"),
             testFailed(testIndex, moduleName, test.label, test.expectedOutput, res)
         }
     },
+    // defines the modules (groups of tests) and the individual tests that make them up
     testModules = [{
         // tests for the full system
         module: "full system",
